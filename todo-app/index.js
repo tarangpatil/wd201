c283d76d4e13b911de96aacbf44d1e79f8a9caf0
Tarang Patil <tarangpatil09@gmail.com>
2024-01-20 16:01:36 +0530

    Mock data for DB

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,3 +1,9 @@
-const app = require("./app");
-
-app.listen(3000);
+(async () => {
+    const app = require("./app");
+    const { Todo } = require("./models");
+    if ((await Todo.getTodos()).length === 0) {
+        await Todo.addTodo({ title: "Do DSA", dueDate: "2023-03-31" });
+        await Todo.addTodo({ title: "Make projects", dueDate: "2023-03-31" });
+    }
+    app.listen(3000);
+})();
